a5423192a250954f5d229aca6a2cd4a9bcc73a69
Janis Karklins <jjankar@gmail.com>
2017-02-15 14:20:02 +0400

    my pack and git ignore img

---
+++ src/janis/MatInOut.java
@@ -0,0 +1,40 @@
+package janis;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+
+import java.io.File;
+import javax.imageio.ImageIO;
+
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+public class MatInOut {
+   public static void main( String[] args ) { 
+   
+      try {
+         System.loadLibrary( Core.NATIVE_LIBRARY_NAME );
+         File input = new File("dice.jpeg");
+         BufferedImage image = ImageIO.read(input);	
+
+         byte[] data = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+         Mat mat = new Mat(image.getHeight(), image.getWidth(), CvType.CV_8UC3);
+         mat.put(0, 0, data);
+
+         Mat mat1 = new Mat(image.getHeight(),image.getWidth(),CvType.CV_8UC1);
+         Imgproc.cvtColor(mat, mat1, Imgproc.DIST_MASK_5);
+
+         byte[] data1 = new byte[mat1.rows() * mat1.cols() * (int)(mat1.elemSize())];
+         mat1.get(0, 0, data1);
+         BufferedImage image1 = new BufferedImage(mat1.cols(),mat1.rows(), BufferedImage.TYPE_BYTE_GRAY);
+         image1.getRaster().setDataElements(0, 0, mat1.cols(), mat1.rows(), data1);
+
+         File ouptut = new File("YY.jpeg");
+         ImageIO.write(image1, "jpeg", ouptut);
+         
+      } catch (Exception e) {
+         System.out.println("Error: " + e.getMessage());
+      }
+   }
+}
